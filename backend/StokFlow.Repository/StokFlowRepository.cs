7161ca9f47a4a4b5bc49167c8f5327f5e4bc4f57
JoaoPedro-Reis <joaoreis.pedro@estudante.ufjf.br>
2026-01-03 16:18:45 -0300

    criada funcao ExecuteProc que faz contato com o banco de dados

--- backend/StokFlow.Repository/StokFlowRepository.cs
+++ backend/StokFlow.Repository/StokFlowRepository.cs
@@ -1,13 +1,39 @@
-﻿using StokFlow.Repository.Interface;
+﻿using Microsoft.Extensions.Configuration;
+using StokFlow.Repository.Interface;
+using System.Data.SqlClient;
 using System.Xml;
 
 namespace StokFlow.Repository
 {
-    public class StokFlowRepository : IStokFlowRepository
+    public class StokFlowRepository(IConfiguration configuration) : IStokFlowRepository
     {
+        public string xmlRetorno = null;
         public string ExecuteProcedure(string proc, XmlNode xml)
         {
-            return "";
+            using SqlConnection connection = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+            connection.Open();
+
+            using SqlCommand command = new SqlCommand($"exec {proc} {(xml is null ? "" : "@xml")}", connection);
+            if(xml is not null) command.Parameters.AddWithValue("@xml", xml.InnerXml);
+
+            using SqlDataReader reader = command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    xmlRetorno = reader[0].ToString();
+                }
+            }
+
+            if (string.IsNullOrEmpty(xmlRetorno))
+            {
+                return null;
+            }
+            else
+            {
+                return xmlRetorno;
+            }
+
         }
     }
 }
